cca93138a466f398a84c2e76b86633548683d3ed
Hong Jing (Jingles) <jinglescode@gmail.com>
2022-07-17 19:18:41 +0800

    try disable ssr

--- demo/pages/_app.tsx
+++ demo/pages/_app.tsx
@@ -2,15 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Metatags, Navbar } from "../components";
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SafeHydrate>
       <Metatags />
       <Navbar />
       <div className="">
         <Component {...pageProps} />
       </div>
-    </>
+    </SafeHydrate>
   );
 }
 
