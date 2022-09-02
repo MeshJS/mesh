5e955d05684daa39f3f53accf3e3cca882a25d4a
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-09-02 15:22:52 +0100

    implement cip 8

--- packages/module/src/core/CIP8.ts
+++ packages/module/src/core/CIP8.ts
@@ -1,75 +1,71 @@
-// import {
-//   AlgorithmId, BigNum, COSESign1Builder,
-//   CBORValue, COSEKey, HeaderMap, Headers, Int,
-//   KeyType, Label, ProtectedHeaderMap,
-// } from '@emurgo/cardano-message-signing-nodejs';
-// import { fromBytes, toBytes } from '@mesh/common/utils';
-// import type { PrivateKey } from './CSL';
+import {
+  AlgorithmId, BigNum, COSESign1Builder,
+  CBORValue, COSEKey, HeaderMap, Headers,
+  Int, KeyType, Label, ProtectedHeaderMap,
+} from '@emurgo/cardano-message-signing-nodejs';
+import { fromBytes, toBytes } from '@mesh/common/utils';
+import type { Address, PrivateKey } from './CSL';
 
-// export const signMessage = (
-//   messageHex: string, signerKey: PrivateKey,
-// ): string => {
-//   const protectedHeaders = HeaderMap.new();
+export const signMessage = (
+  message: Message, signer: Signer,
+): { key: string; signature: string } => {
+  const protectedHeaders = HeaderMap.new();
+  const unprotectedHeaders = HeaderMap.new();
 
-//   protectedHeaders.set_algorithm_id(
-//     Label.from_algorithm_id(AlgorithmId.EdDSA)
-//   );
-//   // protectedHeaders.set_key_id(publicKey.as_bytes()); // Removed to adhere to CIP-30
-//   protectedHeaders.set_header(
-//     Label.new_text('address'),
-//     CBORValue.new_bytes(Buffer.from(address, 'hex'))
-//   );
-//   const protectedSerialized =
-//     ProtectedHeaderMap.new(protectedHeaders);
-//   const unprotectedHeaders = HeaderMap.new();
-//   const headers = Headers.new(
-//     protectedSerialized,
-//     unprotectedHeaders
-//   );
+  protectedHeaders.set_algorithm_id(
+    Label.from_algorithm_id(AlgorithmId.EdDSA),
+  );
 
-//   const coseSign1Builder = COSESign1Builder.new(
-//     headers, toBytes(messageHex), false
-//   );
+  protectedHeaders.set_header(
+    Label.new_text('address'),
+    CBORValue.new_bytes(signer.address.to_bytes()),
+  );
 
-//   const toSign = coseSign1Builder.make_data_to_sign().to_bytes();
+  const headers = Headers.new(
+    ProtectedHeaderMap.new(protectedHeaders),
+    unprotectedHeaders,
+  );
 
-//   const signedSigStruc = signerKey.sign(toSign).to_bytes();
-//   const coseSign1 = coseSign1Builder.build(signedSigStruc);
+  const coseSign1Builder = COSESign1Builder.new(
+    headers, toBytes(message.payload), false,
+  );
 
-//   const key = COSEKey.new(
-//     Label.from_key_type(KeyType.OKP)
-//   );
-//   key.set_algorithm_id(
-//     Label.from_algorithm_id(AlgorithmId.EdDSA)
-//   );
-//   key.set_header(
-//     Label.new_int(
-//       Int.new_negative(BigNum.from_str('1'))
-//     ),
-//     CBORValue.new_int(
-//       Int.new_i32(6) //CurveType.Ed25519
-//     )
-//   ); // crv (-1) set to Ed25519 (6)
-//   key.set_header(
-//     Label.new_int(
-//       Int.new_negative(BigNum.from_str('2'))
-//     ),
-//     CBORValue.new_bytes(signerKey.to_public().as_bytes())
-//   ); // x (-2) set to public key
+  if (message.externalAAD !== undefined) {
+    coseSign1Builder.set_external_aad(
+      toBytes(message.externalAAD),
+    );
+  }
 
-//   return fromBytes(coseSign1.to_bytes());
-// };
+  const dataToSign = coseSign1Builder.make_data_to_sign();
+  const signedSigStructure = signer.key.sign(dataToSign.to_bytes());
+  const coseSign1 = coseSign1Builder.build(signedSigStructure.to_bytes());
 
-// const buildCOSESign1 = (
-//   headers: Headers, messageHex: string, isExternal = false,
-// ) => {
-//   const coseSign1Builder = COSESign1Builder.new(
-//     headers, toBytes(messageHex), isExternal
-//   );
+  const coseKey = COSEKey.new(Label.from_key_type(KeyType.OKP));
 
-//   const toSign = coseSign1Builder.make_data_to_sign().to_bytes();
+  coseKey.set_algorithm_id(Label.from_algorithm_id(AlgorithmId.EdDSA));
 
-//   const signedSigStruc = signerKey.sign(toSign).to_bytes();
-//   const coseSign1 = coseSign1Builder.build(signedSigStruc);
-// };
-export {};
+  coseKey.set_header(
+    Label.new_int(Int.new_negative(BigNum.from_str('1'))),
+    CBORValue.new_int(Int.new_i32(6)),
+  );
+
+  coseKey.set_header(
+    Label.new_int(Int.new_negative(BigNum.from_str('2'))),
+    CBORValue.new_bytes(signer.key.to_public().as_bytes()),
+  );
+
+  return {
+    key: fromBytes(coseKey.to_bytes()),
+    signature: fromBytes(coseSign1.to_bytes()),
+  };
+};
+
+export type Message = {
+  payload: string;
+  externalAAD?: string;
+};
+
+export type Signer = {
+  address: Address;
+  key: PrivateKey;
+};
