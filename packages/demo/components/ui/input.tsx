7240f2a7c261d3f9090f3f0147e97f3aa9196892
Hong Jing (Jingles) <jinglescode@gmail.com>
2022-09-11 17:07:26 +0800

    Update input.tsx

--- packages/demo/components/ui/input.tsx
+++ packages/demo/components/ui/input.tsx
@@ -5,7 +5,15 @@ export default function Input({
   type = 'text',
   className = '',
   disabled = false,
-  label = false,
+  label = undefined,
+}:{
+  value;
+  onChange;
+  placeholder?: string;
+  type?: string;
+  className?: string;
+  disabled?: boolean;
+  label?: undefined | string;
 }) {
   let _classname =
     'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
