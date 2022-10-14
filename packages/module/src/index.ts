b6d9cce615b057d47a2e87b5672216917f26cd96
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-10-14 12:52:43 +0100

    export coin selection algorithm

--- packages/module/src/index.ts
+++ packages/module/src/index.ts
@@ -1,6 +1,7 @@
 export * from './common/contracts';
 export * from './common/types';
 export * from './common/utils/resolver';
+export * from './core/CIP2';
 export * from './providers';
 export * from './scripts';
 export * from './transaction';
