09087c4822f7bb4475678c1d4521041403faefa5
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-10-17 11:08:16 +0100

    implement use wallet hook

---
+++ packages/module/src/react/hooks/useWallet.ts
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { WalletContext } from '@mesh/react/contexts';
+
+export const useWallet = () => {
+  const { connectedWalletName, connectingWallet, connectWallet, disconnect } =
+    useContext(WalletContext);
+
+  if (connectWallet === undefined || disconnect === undefined) {
+    throw new Error(
+      'Can\'t call useWallet outside of the WalletProvider context'
+    );
+  }
+
+  return {
+    name: connectedWalletName,
+    connecting: connectingWallet,
+    connect: connectWallet,
+    disconnect,
+  };
+};
