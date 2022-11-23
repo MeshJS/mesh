d812f3293988b67a903940f026a7bbf3ae5f785f
Akim <gentilakili98@gmail.com>
2022-11-23 23:25:23 +0200

    add yoroi wallet

--- packages/module/src/common/constants.ts
+++ packages/module/src/common/constants.ts
@@ -81,5 +81,5 @@ export const SUPPORTED_HANDLES: Record<number, string> = {
 };
 
 export const SUPPORTED_WALLETS = [
-  'eternl', 'flint', 'nami', 'nufi',
+  'eternl', 'flint', 'nami', 'nufi','yoroi'
 ];
