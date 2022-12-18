1e5e2b3418044eafb014e726a4fa0c4c1defc6c1
Hong Jing (Jingles) <jinglescode@gmail.com>
2022-12-18 21:59:55 +0800

    WIP data sign

--- packages/demo/pages/apis/transaction/index.tsx
+++ packages/demo/pages/apis/transaction/index.tsx
@@ -2,6 +2,7 @@ import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
 import type { NextPage } from 'next';
 import CommonLayout from '../../../components/common/layout';
 import SendAdaHandler from '../../../components/pages/apis/transaction/basic/sendAdaHandler';
+import TxSetMetadata from '../../../components/pages/apis/transaction/basic/setMetadata';
 import CoinSelection from '../../../components/pages/apis/transaction/coinSelection';
 import CommonHero from '../../../components/pages/apis/transaction/commonHero';
 import SendAda from '../../../components/pages/apis/transaction/sendAda';
@@ -15,6 +16,7 @@ const TransactionPage: NextPage = () => {
     { label: 'Send multi-assets to addresses', to: 'sendAssets' },
     { label: 'Send Assets to Handler', to: 'sendAdaHandler' },
     { label: 'Set time limit', to: 'setTimeLimit' },
+    { label: 'Set metadata', to: 'setMetadata' },
     { label: 'Coin Selection', to: 'coinSelection' },
   ];
 
@@ -34,6 +36,7 @@ const TransactionPage: NextPage = () => {
         <SendAssets />
         <SendAdaHandler />
         <SetTimeLimit />
+        <TxSetMetadata />
         <CoinSelection />
       </CommonLayout>
     </>
---
+++ packages/demo/pages/guides/verify-wallet.tsx
@@ -0,0 +1,107 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import GuidesLayout from '../../components/pages/guides/layout';
+import Codeblock from '../../components/ui/codeblock';
+import { Element } from 'react-scroll';
+import Metatags from '../../components/site/metatags';
+
+import { generateNonce, BrowserWallet, checkSignature } from '@meshsdk/core';
+import RunDemoButton from '../../components/common/runDemoButton';
+
+import { CardanoWallet, useWallet } from '@meshsdk/react';
+
+const GuideLoginWithWalletPage: NextPage = () => {
+  const { wallet, connected, name, connecting, connect, disconnect, error } =
+    useWallet();
+
+  const sidebarItems = [{ label: 'System setup', to: 'systemsetup' }];
+
+  async function frontendStep1() {
+    if (connected) {
+      const signerAddress = (await wallet.getRewardAddresses())[0];
+      console.log('signerAddress', signerAddress);
+
+      const nonce = await generateNonce();
+      console.log('nonce', nonce);
+
+      const signature = await wallet.signData(signerAddress, nonce);
+      console.log('signature', signature);
+
+      const result = checkSignature(nonce, signerAddress, signature);
+      console.log('result', result);
+    }
+  }
+
+  async function backendStep1(signerAddress) {
+    const nonce = await generateNonce();
+    console.log(112, nonce);
+  }
+
+  async function frontendStep2(nonce) {}
+
+  async function backendStep2(nonce, signerAddress, signature) {}
+
+  return (
+    <>
+      <Metatags
+        title="cryptographically prove ownership"
+        description="Login"
+        image="/guides/develop-first-web-app.png"
+      />
+      <GuidesLayout
+        title="Cryptographically Prove Wallet Ownership"
+        desc="Cryptographically prove the ownership of a wallet by
+        signing a piece of data using data sign."
+        sidebarItems={sidebarItems}
+        image="/guides/door-gf0710cc4d_640.jpg"
+      >
+        <p>
+          It's cryptographically easy to prove the ownership of an account by
+          signing a piece of data using a private key. Since a user's public
+          address can used as their identifier, we can build an authentication
+          mechanism that is based on message signing. This mechanism is made
+          possible by being able to cryptographically prove the ownership of an
+          account by signing a specific piece of data using the corresponding
+          private key. If the data is correctly signed, then the backend will
+          recognize it as the owner of the public address.
+        </p>
+
+        <p>
+          JSON Web Token (JWT) claims can typically be used to pass identity of
+          authenticated users between an identity provider and a service
+          provider. A server (service provider) could generate a token and provide
+          that to a client (identity provider). The client could then use that
+          token to prove the ownership of the wallet, these tokens can be signed
+          by one party's private key.
+        </p>
+
+        <p>Some usage of data sign to cryptographically prove ownership:</p>
+
+        <ul>
+          <li>
+            <b>Authenticate user sign in using JSON Web Token (JWT)</b>. A
+            cryptographically-secure login to prove the ownership of an account
+            by signing a piece of data using a private key.
+          </li>
+          <li>
+            <b>Authenticate user's action</b>. If the backend wants to confirm
+            user's authorization on a off-chain action, for example, performing
+            in-game trading.
+          </li>
+        </ul>
+
+        <Element name="demo">
+          <h2>Demo</h2>
+          <CardanoWallet />
+          <RunDemoButton
+            runDemoFn={frontendStep1}
+            loading={false}
+            response={false}
+          />
+        </Element>
+      </GuidesLayout>
+    </>
+  );
+};
+
+export default GuideLoginWithWalletPage;
