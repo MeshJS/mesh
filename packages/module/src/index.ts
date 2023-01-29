d6ada5193ff640f1bf674d7e3f30bbfc85634de7
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-01-29 10:20:31 +0100

    export helper functions

--- packages/module/src/index.ts
+++ packages/module/src/index.ts
@@ -1,6 +1,10 @@
 export * from './common/contracts';
 export * from './common/types';
-export { generateNonce } from './common/helpers';
+export {
+  generateNonce,
+  readPlutusData,
+  readTransaction,
+} from './common/helpers';
 export * from './common/utils/resolver';
 export * from './core/CIP2';
 export { checkSignature } from './core/CIP8';
