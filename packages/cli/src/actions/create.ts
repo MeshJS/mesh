f38c8e134aec53c40901ea539d61710466ab62e8
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-02-15 14:02:50 +0100

    pump up version

--- packages/cli/src/actions/create.ts
+++ packages/cli/src/actions/create.ts
@@ -18,20 +18,18 @@ export const create = async (name, options) => {
       { title: 'Multi-Sig Minting', value: 'minting' },
       { title: 'Stake-Pool Website', value: 'staking' },
       { title: 'Cardano Sign-In', value: 'signin' },
-      { title: 'Ecommerce Store', value: 'ecommerce' },
+      { title: 'E-Commerce Store', value: 'ecommerce' },
     ]));
 
   const stack =
     options.stack ??
     (await askUser('What Stack do you want to use?', [
       { title: 'Next.js', value: 'next' },
-      // { title: 'Remix <COMING-SOON>', value: 'remix' },
     ]));
 
   const language =
     options.language ??
     (await askUser('What language do you want to use?', [
-      // { title: 'JavaScript', value: 'js' },
       { title: 'TypeScript', value: 'ts' },
     ]));
 
