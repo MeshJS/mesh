5b9dcd8014260886890b39c6a045d78d2924f9b1
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-02-22 10:35:37 +0100

    lint fix and pump up the version

--- packages/module/src/core/CIP2.ts
+++ packages/module/src/core/CIP2.ts
@@ -25,7 +25,7 @@ export const keepRelevant = (
     ? remainingLovelace(
         requestedLovelace.clamped_sub(selectedLovelace).to_str(),
         initialUTxOSet.filter((iu) => {
-          return !multiAsset.map((su) => su.input.txHash).includes(iu.input.txHash)
+          return !multiAsset.map((su) => su.input.txHash).includes(iu.input.txHash);
         }))
     : [];
 
