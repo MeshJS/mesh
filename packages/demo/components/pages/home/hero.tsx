8b2e122ce541117976189538d02de416841728ec
Hong Jing (Jingles) <jinglescode@gmail.com>
2023-09-22 16:37:51 +0800

    remove catalyst hero section

--- packages/demo/components/pages/home/hero.tsx
+++ packages/demo/components/pages/home/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
     <section className="bg-white dark:bg-gray-900">
       <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16">
         <div className="grid items-center gap-8 mb-8 lg:gap-12 lg:grid-cols-12">
-          {/* <div className="col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
+          <div className="col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
             <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">
               Build applications on Cardano with ease
             </h1>
@@ -26,29 +26,8 @@ export default function Hero() {
                 <ChevronRightIcon className="w-5 h-5 ml-2 -mr-1" />
               </div>
             </Link>
-          </div> */}
-          <div className="col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
-            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">
-              Project Catalyst
-            </h1>
-            <p className="max-w-xl mx-auto mb-6 font-light text-gray-500 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">
-              We have 2 proposals to build up the developer community.
-            </p>
-            <div className="flex flex-col gap-2">
-              <Link href="https://cardano.ideascale.com/c/idea/106261" target='_blank' rel="noopener noreferrer">
-                <div className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 cursor-pointer">
-                  Support community to building Cardano dApps
-                  <ChevronRightIcon className="w-5 h-5 ml-2 -mr-1" />
-                </div>
-              </Link>
-              <Link href="https://cardano.ideascale.com/c/idea/106254" target='_blank' rel="noopener noreferrer">
-                <div className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 cursor-pointer">
-                  Support library, resources & community building
-                  <ChevronRightIcon className="w-5 h-5 ml-2 -mr-1" />
-                </div>
-              </Link>
-            </div>
           </div>
+          
           <div className="hidden lg:block col-span-6">
             {!isSSR ? (
               <video className="w-full" autoPlay muted>
