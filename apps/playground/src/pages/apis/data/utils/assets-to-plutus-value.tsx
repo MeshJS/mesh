688c34b7faf14e503260c61f29e43e5d825dd9e8
SIDANWhatever <wongkahinhinson@gmail.com>
2024-08-03 18:08:00 +0800

    remove obsolete code snippet

--- apps/playground/src/pages/apis/data/utils/assets-to-plutus-value.tsx
+++ apps/playground/src/pages/apis/data/utils/assets-to-plutus-value.tsx
@@ -1,4 +1,4 @@
-import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";
+import { Asset, value, Value } from "@meshsdk/core";
 
 import LiveCodeDemo from "~/components/sections/live-code-demo";
 import TwoColumnsScroll from "~/components/sections/two-columns-scroll";
@@ -9,7 +9,7 @@ export default function DataAssetsToPlutusValue() {
       sidebarTo="DataAssetsToPlutusValue"
       title="Assets to Plutus Value"
       leftSection={Left()}
-      rightSection={Right()}
+      // rightSection={Right()}
     />
   );
 }
@@ -35,109 +35,109 @@ function Left() {
   );
 }
 
-function Right() {
-  return (
-    <>
-      <ADAValue />
-      <TokenValue />
-      <MultipleValue />
-    </>
-  );
-}
+// function Right() {
+//   return (
+//     <>
+//       <ADAValue />
+//       <TokenValue />
+//       <MultipleValue />
+//     </>
+//   );
+// }
 
-function ADAValue() {
-  async function runDemo() {
-    const val: Asset[] = [{ unit: "lovelace", quantity: "1000000" }];
-    const plutusValue: Value = value(val);
-    return plutusValue;
-  }
+// function ADAValue() {
+//   async function runDemo() {
+//     const val: Asset[] = [{ unit: "lovelace", quantity: "1000000" }];
+//     const plutusValue: Value = value(val);
+//     return plutusValue;
+//   }
 
-  let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
-  code += `const val: Asset[] = [{ unit: "lovelace", quantity: "1000000" }];\n\n`;
-  code += `const plutusValue: Value = value(val);\n`;
+//   let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
+//   code += `const val: Asset[] = [{ unit: "lovelace", quantity: "1000000" }];\n\n`;
+//   code += `const plutusValue: Value = value(val);\n`;
 
-  return (
-    <LiveCodeDemo
-      title="Lovelace Value"
-      subtitle="Convert lovelace to Plutus Value"
-      code={code}
-      runCodeFunction={runDemo}
-    />
-  );
-}
+//   return (
+//     <LiveCodeDemo
+//       title="Lovelace Value"
+//       subtitle="Convert lovelace to Plutus Value"
+//       code={code}
+//       runCodeFunction={runDemo}
+//     />
+//   );
+// }
 
-function TokenValue() {
-  async function runDemo() {
-    const val: Asset[] = [
-      {
-        unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",
-        quantity: "345",
-      },
-    ];
-    const plutusValue: Value = value(val);
-    return plutusValue;
-  }
+// function TokenValue() {
+//   async function runDemo() {
+//     const val: Asset[] = [
+//       {
+//         unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",
+//         quantity: "345",
+//       },
+//     ];
+//     const plutusValue: Value = value(val);
+//     return plutusValue;
+//   }
 
-  let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
+//   let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
 
-  code += `const val: Asset[] = [\n`;
-  code += `  {\n`;
-  code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",\n`;
-  code += `    quantity: "345",\n`;
-  code += `  },\n`;
-  code += `];\n\n`;
+//   code += `const val: Asset[] = [\n`;
+//   code += `  {\n`;
+//   code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",\n`;
+//   code += `    quantity: "345",\n`;
+//   code += `  },\n`;
+//   code += `];\n\n`;
 
-  code += `const plutusValue: Value = value(val);\n`;
+//   code += `const plutusValue: Value = value(val);\n`;
 
-  return (
-    <LiveCodeDemo
-      title="Token Value"
-      subtitle="Convert native token to Plutus Value"
-      code={code}
-      runCodeFunction={runDemo}
-    />
-  );
-}
+//   return (
+//     <LiveCodeDemo
+//       title="Token Value"
+//       subtitle="Convert native token to Plutus Value"
+//       code={code}
+//       runCodeFunction={runDemo}
+//     />
+//   );
+// }
 
-function MultipleValue() {
-  async function runDemo() {
-    const val: Asset[] = [
-      { unit: "lovelace", quantity: "1000000" },
-      {
-        unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc817001234",
-        quantity: "567",
-      },
-      {
-        unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",
-        quantity: "345",
-      },
-    ];
-    const plutusValue: Value = value(val);
-    return plutusValue;
-  }
+// function MultipleValue() {
+//   async function runDemo() {
+//     const val: Asset[] = [
+//       { unit: "lovelace", quantity: "1000000" },
+//       {
+//         unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc817001234",
+//         quantity: "567",
+//       },
+//       {
+//         unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",
+//         quantity: "345",
+//       },
+//     ];
+//     const plutusValue: Value = value(val);
+//     return plutusValue;
+//   }
 
-  let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
+//   let code = `import { Asset, parsePlutusValueToAssets, value, Value } from "@meshsdk/core";\n\n`;
 
-  code += `const val: Asset[] = [\n`;
-  code += `  { unit: "lovelace", quantity: "1000000" },\n`;
-  code += `  {\n`;
-  code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc817001234",\n`;
-  code += `    quantity: "567",\n`;
-  code += `  },\n`;
-  code += `  {\n`;
-  code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",\n`;
-  code += `    quantity: "345",\n`;
-  code += `  },\n`;
-  code += `];\n\n`;
+//   code += `const val: Asset[] = [\n`;
+//   code += `  { unit: "lovelace", quantity: "1000000" },\n`;
+//   code += `  {\n`;
+//   code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc817001234",\n`;
+//   code += `    quantity: "567",\n`;
+//   code += `  },\n`;
+//   code += `  {\n`;
+//   code += `    unit: "baefdc6c5b191be372a794cd8d40d839ec0dbdd3c28957267dc8170074657374696e676e657777616c2e616461",\n`;
+//   code += `    quantity: "345",\n`;
+//   code += `  },\n`;
+//   code += `];\n\n`;
 
-  code += `const plutusValue: Value = value(val);\n`;
+//   code += `const plutusValue: Value = value(val);\n`;
 
-  return (
-    <LiveCodeDemo
-      title="Multiple Values"
-      subtitle="Convert multiple assets to Plutus Value"
-      code={code}
-      runCodeFunction={runDemo}
-    />
-  );
-}
+//   return (
+//     <LiveCodeDemo
+//       title="Multiple Values"
+//       subtitle="Convert multiple assets to Plutus Value"
+//       code={code}
+//       runCodeFunction={runDemo}
+//     />
+//   );
+// }
