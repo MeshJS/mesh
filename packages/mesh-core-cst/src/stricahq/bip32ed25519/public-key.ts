f70865ebc0e2efc35c61e0424d660a62f9d8a0a3
TW <tszwai@emurgo.io>
2024-08-23 10:45:03 +0800

    restructure strica imports

---
+++ packages/mesh-core-cst/src/stricahq/bip32ed25519/public-key.ts
@@ -0,0 +1,27 @@
+import { EDDSA } from "./ed25519e/eddsa";
+import { hash28 } from "./utils";
+
+const eddsa = new EDDSA();
+
+class PublicKey {
+  pubKey: Buffer;
+
+  constructor(pubKey: Buffer) {
+    this.pubKey = pubKey;
+  }
+
+  toBytes(): Buffer {
+    return this.pubKey;
+  }
+
+  hash(): Buffer {
+    return hash28(this.pubKey);
+  }
+
+  verify(signature: Buffer, data: Buffer) {
+    const keyPair = eddsa.keyFromPublic(this.pubKey.toString("hex"));
+    return keyPair.verify(data.toString("hex"), signature.toString("hex"));
+  }
+}
+
+export { PublicKey };
