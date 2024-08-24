b803b08f66f188bc929ce2b2542a19b860e51b68
Hong Jing (Jingles) <jinglescode@gmail.com>
2024-08-24 08:01:09 +0800

    Create wrapper.ts

---
+++ packages/mesh-core-cst/src/stricahq/bip32ed25519/wrapper.ts
@@ -0,0 +1,18 @@
+import * as cjsBip32ed25519 from "@stricahq/bip32ed25519";
+
+// This ensures that TypeScript understands the types properly
+export const bip32ed25519: typeof cjsBip32ed25519 & {
+  default?: typeof cjsBip32ed25519;
+} = cjsBip32ed25519;
+const exportedBip32ed25519 = bip32ed25519?.default || bip32ed25519;
+
+// Export individual classes and types
+export type StricaPrivateKey = cjsBip32ed25519.PrivateKey;
+export type StricaPublicKey = cjsBip32ed25519.PublicKey;
+export type StricaBip32PrivateKey = cjsBip32ed25519.Bip32PrivateKey;
+export type StricaBip32PublicKey = cjsBip32ed25519.Bip32PublicKey;
+
+export const StricaPrivateKey = exportedBip32ed25519.PrivateKey;
+export const StricaPublicKey = exportedBip32ed25519.PublicKey;
+export const StricaBip32PrivateKey = exportedBip32ed25519.Bip32PrivateKey;
+export const StricaBip32PublicKey = exportedBip32ed25519.Bip32PublicKey;
