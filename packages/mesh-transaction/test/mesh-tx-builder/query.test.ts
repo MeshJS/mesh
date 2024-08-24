3ddd8a29881eccc53f7b08d81d0e1d2c3d36f6e7
khwt <wongkahinhinson@gmail.com>
2024-08-25 01:58:06 +0800

    feat: adding test case for txbuilder

---
+++ packages/mesh-transaction/test/mesh-tx-builder/query.test.ts
@@ -0,0 +1,95 @@
+import {
+  AccountInfo,
+  IFetcher,
+  MintItem,
+  TransactionInfo,
+  TxIn,
+  UTxO,
+} from "@meshsdk/common";
+import { MeshTxBuilder } from "@meshsdk/transaction";
+
+class MockTxBuilder extends MeshTxBuilder {
+  constructor() {
+    super();
+    this.queriedTxHashes = new Set();
+    this.queriedUTxOs = {};
+    this.fetcher = {
+      fetchUTxOs: jest.fn().mockResolvedValue([]),
+      fetchAccountInfo: jest.fn().mockResolvedValue({} as AccountInfo),
+      fetchAddressUTxOs: jest.fn().mockResolvedValue([]),
+      fetchProtocolParameters: jest.fn().mockResolvedValue({}),
+      fetchTxInfo: jest.fn().mockResolvedValue({} as TransactionInfo),
+      fetchNetworkInfo: jest.fn().mockResolvedValue({}),
+      fetchAssetInfo: jest.fn().mockResolvedValue({}),
+      fetchStakePoolInfo: jest.fn().mockResolvedValue({}),
+      fetchDelegationInfo: jest.fn().mockResolvedValue({}),
+    } as unknown as IFetcher;
+  }
+
+  getUTxOInfoExtended = this.getUTxOInfo;
+  queryAllTxInfoExtended = this.queryAllTxInfo;
+}
+
+describe("MeshTxBuilder", () => {
+  let txBuilder: MockTxBuilder;
+
+  beforeEach(() => {
+    txBuilder = new MockTxBuilder();
+  });
+
+  describe("queryAllTxInfo", () => {
+    it("should call getUTxOInfo for incomplete TxIns", async () => {
+      const incompleteTxIns: TxIn[] = [
+        {
+          type: "PubKey",
+          txIn: { txHash: "txHash1", txIndex: 0 },
+        },
+        {
+          type: "Script",
+          txIn: { txHash: "txHash2", txIndex: 1 },
+          scriptTxIn: {
+            scriptSource: {
+              type: "Inline",
+              txHash: "txHash3",
+              txIndex: 2,
+            },
+          },
+        },
+      ];
+      const incompleteMints: MintItem[] = [];
+
+      const getUTxOInfoMock = jest
+        .spyOn(txBuilder as any, "getUTxOInfo")
+        .mockResolvedValue({} as UTxO);
+
+      await txBuilder.queryAllTxInfoExtended(incompleteTxIns, incompleteMints);
+      expect(getUTxOInfoMock).toHaveBeenCalledWith("txHash1");
+      expect(getUTxOInfoMock).toHaveBeenCalledWith("txHash3");
+    });
+
+    it("should call getUTxOInfo for incomplete Mints", async () => {
+      const incompleteTxIns: TxIn[] = [];
+      const incompleteMints: MintItem[] = [
+        {
+          type: "Plutus",
+          policyId: "policyId1",
+          assetName: "assetName1",
+          amount: "100",
+          scriptSource: {
+            type: "Inline",
+            txHash: "txHash4",
+            txIndex: 3,
+          },
+        },
+      ];
+
+      const getUTxOInfoMock = jest
+        .spyOn(txBuilder as any, "getUTxOInfo")
+        .mockResolvedValue({} as UTxO);
+
+      await txBuilder.queryAllTxInfoExtended(incompleteTxIns, incompleteMints);
+
+      expect(getUTxOInfoMock).toHaveBeenCalledWith("txHash4");
+    });
+  });
+});
