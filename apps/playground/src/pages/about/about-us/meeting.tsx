fb65245db603a4bac260cee4caaeb518d0ab9989
Hong Jing (Jingles) <jinglescode@gmail.com>
2024-11-07 11:02:33 +0800

    update playground docs and add blockfrost koios endpoints

--- apps/playground/src/pages/about/about-us/meeting.tsx
+++ apps/playground/src/pages/about/about-us/meeting.tsx
@@ -6,7 +6,7 @@ export default function AboutMeeting() {
   return (
     <AboutSection
       title="Planning and Open Office"
-      description="We meet every week to discuss the progress of the project and plan the next steps every Tuesday at 1200UTC, where we welcome anyone to join us and ask questions."
+      description="We meet every week to discuss the progress of the project and plan the next steps every Tuesday at 1400UTC, where we welcome anyone to join us and ask questions."
     >
       <>
         <Link href="https://calendar.google.com/calendar/u/0?cid=MmJiNzE0ODRmNjBhZTgyOWZiYTE4MDFjZDlmZTM1N2UwODc5MjlkYmU4Y2U4NGRmYjc3OTgzZjdiODRiZTUxMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t">
