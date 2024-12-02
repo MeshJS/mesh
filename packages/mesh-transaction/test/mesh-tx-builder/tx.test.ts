de08b73f4bb58d08c69c8659f012f235946cc3d6
twwu123 <tszwaiwu.96@gmail.com>
2024-12-02 17:13:17 +0800

    add test for set fee

--- packages/mesh-transaction/test/mesh-tx-builder/tx.test.ts
+++ packages/mesh-transaction/test/mesh-tx-builder/tx.test.ts
@@ -14,6 +14,49 @@ import {
 import { MeshTxBuilder } from "@meshsdk/transaction";
 
 describe("MeshTxBuilder transactions", () => {
+  it("Basic send tx", () => {
+    let mesh = new MeshTxBuilder({ verbose: true });
+    let txHex = mesh
+      .txIn(
+        "2cb57168ee66b68bd04a0d595060b546edf30c04ae1031b883c9ac797967dd85",
+        3,
+        [{ unit: "lovelace", quantity: "9891607895" }],
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+      )
+      .txOut(
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+        [{ unit: "lovelace", quantity: "2000000" }],
+      )
+      .changeAddress(
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+      )
+      .completeSync();
+
+    expect(txHex !== "").toBeTruthy();
+  });
+
+  it("Basic send tx with set fee", () => {
+    let mesh = new MeshTxBuilder();
+    let txHex = mesh
+      .txIn(
+        "2cb57168ee66b68bd04a0d595060b546edf30c04ae1031b883c9ac797967dd85",
+        3,
+        [{ unit: "lovelace", quantity: "9891607895" }],
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+      )
+      .txOut(
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+        [{ unit: "lovelace", quantity: "2000000" }],
+      )
+      .changeAddress(
+        "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+      )
+      .setFee("5000000")
+      .completeSync();
+
+    expect(txHex !== "").toBeTruthy();
+  });
+
   it("Adding embedded datum should produce correct tx cbor", () => {
     let mesh = new MeshTxBuilder();
 
