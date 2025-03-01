f9662289c86249e7b3b96a90550d1129638337fb
twwu123 <tszwaiwu.96@gmail.com>
2025-03-01 23:45:25 +0800

    use blake2b instead of blake2bHex

--- packages/mesh-common/src/utils/data-hash.ts
+++ packages/mesh-common/src/utils/data-hash.ts
@@ -1,6 +1,6 @@
-import { blake2bHex } from "blakejs";
+import { blake2b } from "blakejs";
 
 export const hashDrepAnchor = (jsonLD: object): string => {
-  const jsonHash = blake2bHex(JSON.stringify(jsonLD, null, 2), undefined, 32);
-  return jsonHash;
+  const jsonHash = blake2b(JSON.stringify(jsonLD, null, 2), undefined, 32);
+  return Buffer.from(jsonHash).toString("hex");
 };
