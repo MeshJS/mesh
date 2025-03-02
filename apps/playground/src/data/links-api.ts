531624f6cfc73ac677011a471e899756d38f0aff
Jingles <jinglescode@gmail.com>
2025-03-02 09:53:05 +0800

    update wallet

--- apps/playground/src/data/links-api.ts
+++ apps/playground/src/data/links-api.ts
@@ -4,7 +4,6 @@ import { metaProviders } from "./links-providers";
 import { metaReact } from "./links-react";
 import { metaSmartContract } from "./links-smart-contracts";
 import { metaSvelte } from "./links-svelte";
-import { metaTransaction } from "./links-transactions";
 import { metaTxbuilder } from "./links-txbuilders";
 import { metaUtilities } from "./links-utilities";
 import { metaWallets } from "./links-wallets";
@@ -13,7 +12,6 @@ import { metaYaci } from "./links-yaci";
 export const linksApi: MenuItem[] = [
   metaWallets,
   metaTxbuilder,
-  metaTransaction,
   metaReact,
   metaSvelte,
   metaProviders,
