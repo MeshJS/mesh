cb26773f3ca6e36b6c0e0c3be8460008f74043df
twwu123 <tszwaiwu.96@gmail.com>
2025-03-09 11:43:16 +0900

    test cleanup

--- packages/mesh-transaction/test/mesh-tx-builder/script-tx.test.ts
+++ packages/mesh-transaction/test/mesh-tx-builder/script-tx.test.ts
@@ -15,6 +15,7 @@ import {
   resolveNativeScriptHash,
   resolveScriptHashDRepId,
   resolveScriptRef,
+  Serialization,
 } from "@meshsdk/core-cst";
 
 describe("MeshTxBuilder - Script Transactions", () => {
@@ -798,6 +799,135 @@ describe("MeshTxBuilder - Script Transactions", () => {
     ).toEqual([
       { budget: { mem: 2001, steps: 380149 }, index: 0, tag: "SPEND" },
       { budget: { mem: 2001, steps: 380149 }, index: 1, tag: "SPEND" },
+    ]);
+  });
+
+  it("should be able to mint multiple tokens from same policy id", async () => {
+    const txHex = await txBuilder
+      .mintPlutusScriptV3()
+      .mint("1", alwaysSucceedHash, "60")
+      .mintRedeemerValue("")
+      .mintTxInReference(txHash("tx3"), 0)
+      .mintPlutusScriptV3()
+      .mint("1", alwaysSucceedHash, "61")
+      .mintRedeemerValue("")
+      .mintTxInReference(txHash("tx3"), 0)
+      .txIn(txHash("tx1"), 0)
+      .txInCollateral(txHash("tx1"), 0)
+      .changeAddress(
+        "addr_test1qpvx0sacufuypa2k4sngk7q40zc5c4npl337uusdh64kv0uafhxhu32dys6pvn6wlw8dav6cmp4pmtv7cc3yel9uu0nq93swx9",
+      )
+      .complete();
+
+    const txHex2 = await txBuilder2
+      .mintPlutusScriptV3()
+      .mint("1", alwaysSucceedHash, "60")
+      .mintRedeemerValue("")
+      .mintTxInReference(txHash("tx3"), 0)
+      .mintPlutusScriptV3()
+      .mint("1", alwaysSucceedHash, "61")
+      .mintRedeemerValue("")
+      .mintTxInReference(txHash("tx3"), 0)
+      .txIn(txHash("tx1"), 0)
+      .txInCollateral(txHash("tx1"), 0)
+      .changeAddress(
+        "addr_test1qpvx0sacufuypa2k4sngk7q40zc5c4npl337uusdh64kv0uafhxhu32dys6pvn6wlw8dav6cmp4pmtv7cc3yel9uu0nq93swx9",
+      )
+      .complete();
+
+    expect(
+      await offlineEvaluator.evaluateTx(
+        txHex,
+        Object.values(
+          txBuilder.meshTxBuilderBody.inputsForEvaluation,
+        ) as UTxO[],
+        txBuilder.meshTxBuilderBody.chainedTxs,
+      ),
+    ).toEqual([
+      { budget: { mem: 2001, steps: 380149 }, index: 0, tag: "MINT" },
+    ]);
+    expect(
+      await offlineEvaluator.evaluateTx(
+        txHex2,
+        Object.values(
+          txBuilder.meshTxBuilderBody.inputsForEvaluation,
+        ) as UTxO[],
+        txBuilder.meshTxBuilderBody.chainedTxs,
+      ),
+    ).toEqual([
+      { budget: { mem: 2001, steps: 380149 }, index: 0, tag: "MINT" },
+    ]);
+  });
+
+  it("should be able to perform multiple certificate actions", async () => {
+    const drep: DRep = {
+      alwaysAbstain: null,
+    };
+
+    const txHex = await txBuilder
+      .voteDelegationCertificate(
+        drep,
+        serializeRewardAddress(alwaysSucceedHash, true),
+      )
+      .certificateTxInReference(txHash("tx3"), 0, undefined, undefined, "V3")
+      .certificateRedeemerValue("")
+      .delegateStakeCertificate(
+        serializeRewardAddress(alwaysSucceedHash, true),
+        "624fcb718d9facc7dcc8daa3c5cad6753886ddc968cc4a6bea5e9cc3",
+      )
+      .certificateTxInReference(txHash("tx3"), 0, undefined, undefined, "V3")
+      .certificateRedeemerValue("")
+      .txIn(txHash("tx1"), 0)
+      .txInCollateral(txHash("tx1"), 0)
+      .changeAddress(
+        "addr_test1qpvx0sacufuypa2k4sngk7q40zc5c4npl337uusdh64kv0uafhxhu32dys6pvn6wlw8dav6cmp4pmtv7cc3yel9uu0nq93swx9",
+      )
+      .complete();
+
+    const txHex2 = await txBuilder2
+      .voteDelegationCertificate(
+        drep,
+        serializeRewardAddress(alwaysSucceedHash, true),
+      )
+      .certificateTxInReference(txHash("tx3"), 0, undefined, undefined, "V3")
+      .certificateRedeemerValue("")
+      .delegateStakeCertificate(
+        serializeRewardAddress(alwaysSucceedHash, true),
+        "pool1vf8ukuvdn7kv0hxgm23utjkkw5ugdhwfdrxy56l2t6wvxezspyu",
+      )
+      .certificateTxInReference(txHash("tx3"), 0, undefined, undefined, "V3")
+      .certificateRedeemerValue("")
+      .txIn(txHash("tx1"), 0)
+      .txInCollateral(txHash("tx1"), 0)
+      .changeAddress(
+        "addr_test1qpvx0sacufuypa2k4sngk7q40zc5c4npl337uusdh64kv0uafhxhu32dys6pvn6wlw8dav6cmp4pmtv7cc3yel9uu0nq93swx9",
+      )
+      .complete();
+
+    expect(
+      await offlineEvaluator.evaluateTx(
+        txHex,
+        Object.values(
+          txBuilder.meshTxBuilderBody.inputsForEvaluation,
+        ) as UTxO[],
+        txBuilder.meshTxBuilderBody.chainedTxs,
+      ),
+    ).toEqual([
+      { budget: { mem: 2001, steps: 380149 }, index: 0, tag: "CERT" },
+      { budget: { mem: 2001, steps: 380149 }, index: 1, tag: "CERT" },
+    ]);
+
+    expect(
+      await offlineEvaluator.evaluateTx(
+        txHex2,
+        Object.values(
+          txBuilder.meshTxBuilderBody.inputsForEvaluation,
+        ) as UTxO[],
+        txBuilder.meshTxBuilderBody.chainedTxs,
+      ),
+    ).toEqual([
+      { budget: { mem: 2001, steps: 380149 }, index: 0, tag: "CERT" },
+      { budget: { mem: 2001, steps: 380149 }, index: 1, tag: "CERT" },
     ]);
   });
 });
