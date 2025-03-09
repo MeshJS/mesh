c08503ad17efead3c3ad405c31e6897fcd900fcc
lisicky <lisicky@inbox.ru>
2025-03-09 18:55:28 +0100

    chore: fix test

--- packages/mesh-core-csl/test/core/builder.test.ts
+++ packages/mesh-core-csl/test/core/builder.test.ts
@@ -93,10 +93,14 @@ describe("Builder", () => {
       signingKey: [],
       network: "preprod",
       votes: [],
+      fee: "168141",
+      chainedTxs: [],
+      inputsForEvaluation: {},
+      expectedByronAddressWitnesses: [],
+      expectedNumberKeyWitnesses: 0,
     };
     const txHex = serializer.serializeTxBody(body, DEFAULT_PROTOCOL_PARAMETERS);
-    expect(txHex).toEqual(
-      "84a300d90102818258201662c4b349907e4d92e0995fd9dcdc9a4489f7dff4f5cce6b4b3901de479308c0e0182825839001e4eb194e3335a0dcc4f5c5d009318167c583bb3b0879d9f718cd9e0d63a93470bd4d8bb986c02ff8a6043796b91cc397ceb29058f5c9ac01a0012c97f825839001e4eb194e3335a0dcc4f5c5d009318167c583bb3b0879d9f718cd9e0d63a93470bd4d8bb986c02ff8a6043796b91cc397ceb29058f5c9ac01a2e16c2ec021a0002917da0f5f6",
-    );
+    const expectedTxHex = "84a300d90102818258201662c4b349907e4d92e0995fd9dcdc9a4489f7dff4f5cce6b4b3901de479308c0e0182825839001e4eb194e3335a0dcc4f5c5d009318167c583bb3b0879d9f718cd9e0d63a93470bd4d8bb986c02ff8a6043796b91cc397ceb29058f5c9ac01a0012c97f825839001e4eb194e3335a0dcc4f5c5d009318167c583bb3b0879d9f718cd9e0d63a93470bd4d8bb986c02ff8a6043796b91cc397ceb29058f5c9ac01a2e16c39c021a000290cda0f5f6";
+    expect(expectedTxHex).toEqual(txHex);
   });
 });
