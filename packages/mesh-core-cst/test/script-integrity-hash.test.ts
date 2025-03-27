69a06bb2dfc8a30da127346efb356e4207c9e391
twwu123 <tszwaiwu.96@gmail.com>
2025-03-27 13:44:38 +0800

    update script integrity tests

--- packages/mesh-core-cst/test/script-integrity-hash.test.ts
+++ packages/mesh-core-cst/test/script-integrity-hash.test.ts
@@ -1,3 +1,4 @@
+import { setInConwayEra } from "@cardano-sdk/core";
 import { Hash32ByteBase16 } from "@cardano-sdk/crypto";
 import { HexBlob } from "@cardano-sdk/util";
 
@@ -7,6 +8,10 @@ import { RedeemerPurpose, Serialization } from "../src";
 import { hashScriptData } from "../src/utils/script-data-hash";
 
 describe("ScriptIntegrityHash", () => {
+  beforeAll(() => {
+    setInConwayEra(true);
+  });
+
   const redeemer0 = {
     index: 0,
     data: Serialization.PlutusData.fromCbor(
@@ -30,24 +35,25 @@ describe("ScriptIntegrityHash", () => {
     },
   };
 
-  let redeemers = [redeemer0, redeemer1].map((redeemer) => {
-    return Serialization.Redeemer.fromCore(redeemer);
-  });
+  let redeemers = Serialization.Redeemers.fromCore([redeemer0, redeemer1]);
 
   const datum0 = Serialization.PlutusData.fromCbor(
     HexBlob(
       "d8799f5820e0628178ceac8f426c13f080db01464c670b2c79a35c9eb7f2617b424b0f9d7200ff",
     ),
   );
-  const datums = [datum0];
+  const datums = Serialization.CborSet.fromCore(
+    [datum0.toCore()],
+    Serialization.PlutusData.fromCore,
+  );
+
+  let costModels = new Serialization.Costmdls();
+  let costModelV3 = Serialization.CostModel.newPlutusV3(
+    DEFAULT_V3_COST_MODEL_LIST,
+  );
+  costModels.insert(costModelV3);
 
   it("should calculate the correct hash for v3 scripts", async () => {
-    let costModels = new Serialization.Costmdls();
-    let costModelV3 = Serialization.CostModel.newPlutusV3(
-      DEFAULT_V3_COST_MODEL_LIST,
-    );
-    costModels.insert(costModelV3);
-
     const scriptDataHash = hashScriptData(costModels, redeemers);
     expect(scriptDataHash).toEqual(
       Hash32ByteBase16(
@@ -57,16 +63,10 @@ describe("ScriptIntegrityHash", () => {
   });
 
   it("should calculate the correct hash when there are datums", async () => {
-    let costModels = new Serialization.Costmdls();
-    let costModelV3 = Serialization.CostModel.newPlutusV3(
-      DEFAULT_V3_COST_MODEL_LIST,
-    );
-    costModels.insert(costModelV3);
-
     const scriptDataHash = hashScriptData(costModels, redeemers, datums);
     expect(scriptDataHash).toEqual(
       Hash32ByteBase16(
-        "d3883c214f7d7b5a03e8982c2ce16c8acad5cd6d7e9ab3d256343ffbea239001",
+        "e4003e3d4453734b7ee5a63da479da5a06174da8832537e1f8272a86e86ea2e0",
       ),
     );
   });
@@ -81,14 +81,17 @@ describe("ScriptIntegrityHash", () => {
     const scriptDataHash = hashScriptData(costModels, undefined, datums);
     expect(scriptDataHash).toEqual(
       Hash32ByteBase16(
-        "9df6d7109e989e1e7276f517a87031e109bbfbe808b0ae994b3e3996acebc895",
+        "434b8b790f1d8d196331515271ea7a9ec952b24846b5250d60ace39a83318829",
       ),
     );
   });
 
   it("should calculate the correct hash with multiple datums", async () => {
     const datum1 = Serialization.PlutusData.fromCbor(HexBlob("40"));
-    const datums = [datum0, datum1];
+    const datums = Serialization.CborSet.fromCore(
+      [datum0.toCore(), datum1.toCore()],
+      Serialization.PlutusData.fromCore,
+    );
 
     let costModels = new Serialization.Costmdls();
     let costModelV3 = Serialization.CostModel.newPlutusV3(
@@ -99,7 +102,39 @@ describe("ScriptIntegrityHash", () => {
     const scriptDataHash = hashScriptData(costModels, redeemers, datums);
     expect(scriptDataHash).toEqual(
       Hash32ByteBase16(
-        "372b41e538bcdcfe8ae62f613b18ae34f620391fa626a8887265f00805cd4c18",
+        "6f32d12156681ae2d87bb002b5bbdd89077c0a415b051e3d9c19fac066a058b2",
+      ),
+    );
+  });
+
+  it("should calculate the correct hash when there are datums 2", async () => {
+    const testDatum = Serialization.PlutusData.fromCbor(HexBlob("d87980"));
+
+    const testRedeemers = Serialization.Redeemers.fromCore([
+      {
+        index: 0,
+        data: Serialization.PlutusData.newBytes(
+          Buffer.from("", "hex"),
+        ).toCore(),
+        purpose: RedeemerPurpose.spend,
+        executionUnits: {
+          memory: 2201,
+          steps: 418163,
+        },
+      },
+    ]);
+
+    const scriptDataHash = hashScriptData(
+      costModels,
+      testRedeemers,
+      Serialization.CborSet.fromCore(
+        [testDatum.toCore()],
+        Serialization.PlutusData.fromCore,
+      ),
+    );
+    expect(scriptDataHash).toEqual(
+      Hash32ByteBase16(
+        "78985bcb429aa3ae4a899695d69b59146d99c15b64b09c5c03862c7b2767c6e3",
       ),
     );
   });
