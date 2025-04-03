ef5f274ed67eb66424f5e1bb4aa1b223e5f042e3
twwu123 <tszwaiwu.96@gmail.com>
2025-04-03 14:14:16 +0800

    add tests for resolving fetched datums

--- packages/mesh-transaction/test/test-util.ts
+++ packages/mesh-transaction/test/test-util.ts
@@ -16,3 +16,9 @@ export const alwaysFailCbor = applyCborEncoding(
 );
 
 export const alwaysFailHash = resolveScriptHash(alwaysFailCbor, "V3");
+
+export const spend42Cbor = applyCborEncoding(
+  "587901010029800aba2aba1aab9eaab9dab9a4888896600264653001300600198031803800cc0180092225980099b8748008c01cdd500144c8cc896600266e1d2000300a375400313370e6eb4c030c02cdd5000a40a916402460140026014601600260106ea800a29450060c018004c00cdd5003452689b2b20021",
+);
+
+export const spend42Hash = resolveScriptHash(spend42Cbor, "V3");
