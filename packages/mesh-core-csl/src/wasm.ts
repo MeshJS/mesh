6d213b86494157a44314ef9b7a904934e3f07cdb
twwu123 <tszwaiwu.96@gmail.com>
2025-06-09 11:18:13 +0800

    fix CSL serializer error handling for evaluation results

--- packages/mesh-core-csl/src/wasm.ts
+++ packages/mesh-core-csl/src/wasm.ts
@@ -2,7 +2,7 @@ import { csl } from "./deser/csl";
 
 export const parseWasmResult = (result: csl.WasmResult): string => {
   if (result.get_status() !== "success") {
-    throw new Error(result.get_data());
+    throw new Error(result.get_error());
   }
   return result.get_data();
 };
