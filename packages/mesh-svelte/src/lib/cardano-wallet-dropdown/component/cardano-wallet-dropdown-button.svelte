604a33c41a20f51af01b8831f02d71846e99bf3f
gf <gianluccaf@outlook.com>
2025-07-16 12:08:30 -0300

    fix typo

--- packages/mesh-svelte/src/lib/cardano-wallet-dropdown/component/cardano-wallet-dropdown-button.svelte
+++ packages/mesh-svelte/src/lib/cardano-wallet-dropdown/component/cardano-wallet-dropdown-button.svelte
@@ -80,9 +80,9 @@
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M19 9l-7 7-7-7"
       />
     </svg>
