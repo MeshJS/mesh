6c7455f593f57e7fb8e7dbb34d4e5e723e8cd975
Emmanuel .A <asaoluemma@gmail.com>
2025-07-22 03:06:17 +0100

    improve guides and steps

--- apps/playground/src/pages/hydra/tutorial/prerequisites.tsx
+++ apps/playground/src/pages/hydra/tutorial/prerequisites.tsx
@@ -14,19 +14,34 @@ export default function HydraTutorialPrerequisites() {
 function Left() {
   return (
     <>
-      <p>
-        First, generate Cardano key pairs and addresses for both participants to
-        identify the hydra-node and manage funds on layer 1:
-      </p>
       <ul>
         <li>
-          Hydra node and cardano-node running, check{" "}
-          <Link href="https://hydra.family/head-protocol/docs/installation/">
-            Installation
-          </Link>
-          .
+          A running cardano node is required to access{" "}
+          <code>cardano-cli</code>{" "}
+        </li>
+        <li>
+          A <code>Hydra-node</code>
+        </li>
+        <li>Another participant following this tutorial (recommended), or</li>
+        <li>Access to two such machines, or</li>
+        <li>
+          100 test ada per participant in a wallet on the <code>preprod</code>{" "}
+          network
         </li>
       </ul>
+      <ul>
+        Hydra-node and cardano-node running, check{" "}
+        <Link href="https://hydra.family/head-protocol/docs/tutorial#step-0-installation">
+          Installation
+        </Link>
+        .
+      </ul>
+      You could also access a pre-built Docker image for a devnet{" "}
+      <code>cardano-node</code> and <code>Hydra-node</code>
+      for quickly following this tutorial. Check the setup{" "}
+      <Link href="https://hydra.family/head-protocol/docs/getting-started">
+        here
+      </Link>
     </>
   );
 }
