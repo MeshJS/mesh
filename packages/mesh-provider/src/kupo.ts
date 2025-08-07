3d5700eef23569acc2fd7f6839fd63fce190b693
twwu123 <tszwaiwu.96@gmail.com>
2025-08-07 11:01:59 +0800

    add method for fetching address utxos

--- packages/mesh-provider/src/kupo.ts
+++ packages/mesh-provider/src/kupo.ts
@@ -13,6 +13,11 @@ import {
   UTxO,
 } from "@meshsdk/common";
 
+import { parseHttpError } from "./utils";
+
+// Unfortunately, Kupo is specifically used for fetching UTxOs and does not implement all methods of the IFetcher interface.
+// Kupo is a specialized service for indexing Transaction outputs (UTxOs) and does not provide all the
+// necessary methods for a full IFetcher implementation.
 export class KupoProvider implements IFetcher {
   baseUrl: string;
   private readonly _axiosInstance: AxiosInstance;
@@ -25,8 +30,33 @@ export class KupoProvider implements IFetcher {
   fetchAccountInfo(address: string): Promise<AccountInfo> {
     throw new Error("Method not implemented.");
   }
-  fetchAddressUTxOs(address: string, asset?: string): Promise<UTxO[]> {
-    throw new Error("Method not implemented.");
+  async fetchAddressUTxOs(address: string, asset?: string): Promise<UTxO[]> {
+    const { status, data } = await this.get(address);
+    if (status === 200) {
+      return data
+        .map((utxo: any) => {
+          const meshValue: Asset[] = this.kupoValueToMeshValue(utxo.value);
+          if (asset) {
+            if (!meshValue.some((v) => v.unit === asset)) {
+              return null; // Skip UTxO if it does not contain the specified asset
+            }
+          }
+          return {
+            input: {
+              txHash: utxo.transaction_id,
+              outputIndex: utxo.output_index,
+            },
+            output: {
+              address: utxo.address,
+              amount: this.kupoValueToMeshValue(utxo.value),
+            },
+          };
+        })
+        .filter((utxo: any) => utxo !== null) as UTxO[];
+    } else {
+      parseHttpError(data);
+    }
+    return [];
   }
   fetchAddressTxs(
     address: string,
@@ -70,10 +100,12 @@ export class KupoProvider implements IFetcher {
           },
           output: {
             address: utxo.address,
-            amount: [],
+            amount: this.kupoValueToMeshValue(utxo.value),
           },
         };
       }) as UTxO[];
+    } else {
+      parseHttpError(data);
     }
     return [];
   }
@@ -89,5 +121,28 @@ export class KupoProvider implements IFetcher {
     } catch (error) {
       throw new Error(`Failed to fetch data from Kupo: ${error}`);
     }
+  }
+
+  private kupoValueToMeshValue(value: any): any {
+    if (value === null || value === undefined) return value;
+    let meshValue: Asset[] = [];
+    meshValue.push({
+      unit: "lovelace",
+      quantity: value.coins,
+    });
+    for (const [assetClass, quantity] of Object.entries(value.assets)) {
+      let [policyId, assetName] = assetClass.split(".");
+      if (!policyId) {
+        throw new Error("Invalid asset class format found");
+      }
+      if (!assetName) {
+        assetName = "";
+      }
+      meshValue.push({
+        unit: policyId + assetName,
+        quantity: String(quantity),
+      });
+    }
+    return meshValue;
   }
 }
